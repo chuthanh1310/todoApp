5d866fe7ccf79db4039269530948084f49dd00e7
chuthanh1310 <chuthanh1310@gmail.com>
2025-06-22 21:55:43 +0700

    to do app AutoMapper

---
+++ AutoMapperMVC/AutoMapperMVC/Models/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace AutoMapperMVC.Models
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Todo, TodoViewModels>().ReverseMap();
+        }
+    }
+}
---
+++ AutoMapperMVC/AutoMapperMVC/Models/TodoViewModels.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoMapperMVC.Models
+{
+    public class TodoViewModels
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+    }
+}
