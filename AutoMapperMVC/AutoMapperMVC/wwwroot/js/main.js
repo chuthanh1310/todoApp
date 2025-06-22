1b48a4fb7226c5d36dcf015d04deb1cc5c42ace8
chuthanh1310 <chuthanh1310@gmail.com>
2025-06-23 04:38:05 +0700

    ..

---
+++ AutoMapperMVC/AutoMapperMVC/wwwroot/js/main.js
@@ -0,0 +1,12 @@
+﻿require.config({
+    baseUrl: "/Scripts",
+    paths: {
+        jquery: "jquery-3.6.0.min"
+    }
+});
+
+require(["jquery"], function ($) {
+    $(function () {
+        $("#todoList").append("<li></li>");
+    });
+});
